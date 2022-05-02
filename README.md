b0db1c697b9c8232bb5186f82e869379d23eebbb
Jeremy Fergason <jeremy.fergason@corwynn.com>
2022-05-01 18:23:12 -0700

    add default types

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # pvlib
+
+pvlib is a helper library for interfacing with the pv-api REST
+API. It provides go types for serializing to/from colfer and methods
+for uploading/downloading from pv-api.
+
+# usage
+
+```bash
+go get github.com/penny-vault/pvlib
+```
+
+```go
+pvlib.Authorize("<API Key>")
+pvlib.FindPortfolio(pvlib.SearchCriteria{
+    Name: "Name",
+    Comparator: pvlib.EQUAL,
+    StringValue: "My Portfolio"
+})
+```
---
+++ pvlib.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2022
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pvlib
+
+import (
+	"errors"
+
+	"github.com/rs/zerolog/log"
+)
+
+type CompareOperator string
+
+const (
+	Equal       CompareOperator = "eq"
+	GreaterThan CompareOperator = "gt"
+	LessThan    CompareOperator = "lt"
+	Like        CompareOperator = "like"
+)
+
+type SearchCriteria struct {
+	Name        *string
+	StringValue *string
+	IntValue    *int
+	Comparator  CompareOperator
+}
+
+var apiKey string = "<not-set>"
+
+func Authorize(myApiKey string) {
+	apiKey = myApiKey
+}
+
+func Get(id []byte) (*Portfolio, error) {
+	return nil, errors.New("not implemented")
+}
+
+// UploadTransactions uploads the provided transactions to the
+// portfolio
+func (p *Portfolio) UploadTransactions(transactions []*Transaction) error {
+	log.Info().Msg("not implemented")
+	return errors.New("not implemented")
+}
